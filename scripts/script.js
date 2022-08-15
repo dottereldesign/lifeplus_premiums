02570110e41dda3cb968619ff47312a146e1cd4d
Jamie Wilson <howemanning@gmail.com>
2022-08-15 14:58:39 +1200

    Add no function to premiums.

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+function lessthanFunction() {
+  document
+    .getElementById("tt__premium")
+    .setAttribute("value", "HCSTT13 - TRAVEL TIME LESS THAN 15 KMS");
+  document.getElementById("tt__quantity").setAttribute("value", "1");
+
+  document
+    .getElementById("td__premium")
+    .setAttribute("value", "TRAVEL DISTANCE ( LESS THAN 15 KMS )");
+  document.getElementById("td__quantity").setAttribute("value", "1");
+}
